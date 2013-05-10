b1b0fab9d28b55f12553ded845a9463468148ddf
JesseLivezey <jesse.livezey@gmail.com>
2013-05-09 18:00:59 -0700

    Cleaned up pca and added reconstitute function to bring back to original size

--- pca.py
+++ pca.py
@@ -5,18 +5,19 @@ from scipy import linalg
 #function to do pca on an array of vectors then reduce dimensionality. Always converts data into columns of data
 def pca(vectors,dim,rowColumn):
     if rowColumn == 'r':
-        vectors = vectors.transpose()
+        vectors2 = vectors.T
+    else:
+        vectors2 = vectors
 
-    centerVecs = vectors.copy()
-    vectorsShape = vectors.shape
+    centerVecs = np.zeros(vectors2.shape)
 #Mean of each row
-    meanVec = vectors.mean(axis=1)
+    meanVec = vectors2.mean(axis=1)
 #Subtract mean
-    for i in range(vectorsShape[0]):
-        for j in range(vectorsShape[1]):
-            centerVecs[i,j] = vectors[i,j]-meanVec[i]
+    for i in xrange(vectors2.shape[0]):
+        for j in xrange(vectors2.shape[1]):
+            centerVecs[i,j] = vectors2[i,j]-meanVec[i]
 #Compute covariance matrix
-    covMat = np.dot(centerVecs,centerVecs.transpose())
+    covMat = np.dot(centerVecs,centerVecs.T)
 #Compute eigen info
     eigen = linalg.eigh(covMat)
     eValues = eigen[0]
@@ -25,14 +26,19 @@ def pca(vectors,dim,rowColumn):
     eValues = eValues[idx]
     eVectors = eVectors[:,idx]
 #Project back onto a reduced dimensionality basis
-    reducedDim = np.zeros(vectors.shape)
+    reducedDim = np.array([np.dot(np.array([eVectors[:,-1-ii]]),centerVecs)[0] for ii in xrange(dim)])
 
-    for i in range(dim):
-        reducedDim = reducedDim+np.dot(sp.array([eVectors[:,-1-i]]).T,np.dot(sp.array([eVectors[:,-1-i]]),centerVecs))
 #Add mean
-    for i in range(vectorsShape[0]):
-        for j in range(vectorsShape[1]):
-            reducedDim[i,j] += meanVec[i]
     if rowColumn == 'r':
-        reducedDim = reducedDim.transpose()
-    return (reducedDim,eValues,eVectors)
+        reducedDim = reducedDim.T
+    return (reducedDim,eValues,eVectors,meanVec)
+
+def reconst(reducedDim,eVectors,meanVec):
+    fullDim = np.zeros((eVectors.shape[1],reducedDim.shape[1]))
+    for ii in xrange(reducedDim.shape[1]):
+        for jj in xrange(reducedDim.shape[0]):
+            fullDim[:,ii] += eVectors[:,-1-jj]*reducedDim[jj,ii]
+    for ii in xrange(reducedDim.shape[1]):
+        fullDim[:,ii] += meanVec
+    return fullDim
+
