8c7ee79b7a00d2fe18860e6bd258b5fc52a8f08d
JesseLivezey <jesse.livezey@gmail.com>
2013-07-18 16:34:57 -0700

    more cleaning

--- pca.py
+++ pca.py
@@ -1,6 +1,6 @@
 import numpy as np
-import scipy as sp
 from scipy import linalg
+import sys
 
 #function to do pca on an array of vectors then reduce dimensionality. Always converts data into columns of data
 def pca(vectors,dim,rowColumn=None):
@@ -8,14 +8,16 @@ def pca(vectors,dim,rowColumn=None):
         vectors2 = vectors
     elif rowColumn == 'r':
         vectors2 = vectors.T
+    else:
+        print 'Malformed rowColumn flag.'
+        sys.exit()
 
     centerVecs = np.zeros(vectors2.shape)
 #Mean of each row
     meanVec = vectors2.mean(axis=1)
 #Subtract mean
-    for i in xrange(vectors2.shape[0]):
-        for j in xrange(vectors2.shape[1]):
-            centerVecs[i,j] = vectors2[i,j]-meanVec[i]
+    for i in xrange(vectors2.shape[1]):
+        centerVecs[:,i] = vectors2[:,i]-meanVec
 #Compute covariance matrix
     covMat = np.dot(centerVecs,centerVecs.T)
 #Compute eigen info
@@ -28,18 +30,20 @@ def pca(vectors,dim,rowColumn=None):
 #Project back onto a reduced dimensionality basis
     reducedDim = np.array([np.dot(np.array([eVectors[:,-1-ii]]),centerVecs)[0] for ii in xrange(dim)])
 
-#Tranpose back to original
+#Transpose back to original
     if rowColumn == 'r':
         reducedDim = reducedDim.T
     return (reducedDim,eValues,eVectors,meanVec)
 
-def reconst(reducedDim,eVectors,meanVec,rowColumn):
-    if rowColumn == 'r':
+def reconst(reducedDim,eVectors,meanVec,rowColumn=None):
+    if rowColumn is None:
+        reducedDim2 = reducedDim
+    elif rowColumn == 'r':
         reducedDim2 = reducedDim.T
     else:
-        reducedDim2 = reducedDim
+        print 'Malformed rowColumn flag.'
+        sys.exit()
     fullDim = np.zeros((eVectors.shape[1],reducedDim2.shape[1]))
-    print fullDim.shape
     for ii in xrange(reducedDim2.shape[1]):
         for jj in xrange(reducedDim2.shape[0]):
             fullDim[:,ii] += eVectors[:,-1-jj]*reducedDim2[jj,ii]
