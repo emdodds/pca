d8da8a1cae755a5645b103e262fc5ca5ca5b9a2d
Jesse Livezey <jlivezey@audience.com>
2014-05-30 09:15:59 -0700

    pca runs, not tested

--- pca.py
+++ pca.py
@@ -11,7 +11,7 @@ class PCA(object):
         Args:
             data: Data to do pca on.
             dim: Dimensionality to reduce to.
-            rowColumn: Flag that specifies how data is formatted.
+            row_col: Flag that specifies how data is formatted.
             whiten: Flag that tell pca to whiten the data before return. Default is False
 
         Returns:
@@ -28,7 +28,7 @@ class PCA(object):
         elif row_col == 'r':
             pass
         else:
-            raise ValueError('Malformed rowColumn flag.')
+            raise ValueError('Malformed row_col flag.')
         if dim is None:
             self.dim = data.shape[1]
         else:
@@ -39,11 +39,10 @@ class PCA(object):
     #Subtract mean
         center_vecs = data-self.mean_vec[np.newaxis,:]
     #Compute SVD
-        print 'Calculating SVD'
         u,self.sValues,self.v = np.linalg.svd(center_vecs, full_matrices=1, compute_uv=1)
-        self.idx = np.argsort(sValues)
+        idx = np.argsort(self.sValues)
         self.sValues = self.sValues[idx][::-1]
-        self.eVectors = self.v[self.idx][::-1]
+        self.eVectors = self.v[idx][::-1]
         self.ready = True
         return
 
@@ -53,7 +52,7 @@ class PCA(object):
         Args:
             data: Data to do pca on.
             dim: Dimensionality to reduce to.
-            rowColumn: Flag that specifies how data is formatted.
+            row_col: Flag that specifies how data is formatted.
             whiten: Flag that tell pca to whiten the data before return. Default is False
 
         Returns:
@@ -77,7 +76,7 @@ class PCA(object):
             dim: Dimensionality to reduce to.
             eValues: Eigenvalues from PCA basis
             eVectors: Eigenvectors, PCA basis
-            rowColumn: Flag that specifies how data is formatted.
+            row_col: Flag that specifies how data is formatted.
             whiten: Flag that tell pca to whiten the data before return. Default is False
 
         Returns:
@@ -96,7 +95,7 @@ class PCA(object):
         elif row_col == 'r':
             pass
         else:
-            raise ValueError('Malformed rowColumn flag.')
+            raise ValueError('Malformed row_col flag.')
         if dim is None:
             self.dim = data.shape[1]
         else:
@@ -105,7 +104,7 @@ class PCA(object):
             whiten = self.whiten
 
     #Subtract mean
-        center_vecs = data-np.array([self.mean_vec]).T
+        center_vecs = data-self.mean_vec[np.newaxis,:]
     #Project onto reduced number of eigenvectors.
         print 'Projecting onto reduced dimensionality basis'
         reduced_dim = data.dot(self.eVectors.T)
@@ -115,41 +114,41 @@ class PCA(object):
             wm = np.diag(1./self.sValues)
             reduced_dim = reduced_dim.dot(wm[:dim,:dim])
     #Transpose back to original
-        if row_col == 'r':
+        if row_col == 'c':
             reduced_dim = reduced_dim.T
         return reduced_dim
 
-    def inv_transform(data, row_col='r', whiten=None):
+    def inv_transform(self, data, row_col='r', whiten=None):
         """Takes vectors from reduced dimensionality basis and returns them to full dimensionality basis.
 
         Args:
             reducedDim: Vectors of reduced dimensionality.
             eVectors: Original basis vectors.
             meanVec: Mean of original vectors.
-            rowColumn: Flag that determines data format.
+            row_col: Flag that determines data format.
 
         Returns:
             fullDim: Vectors in full dimensionality.
 
         Raises:
-           ValueError: rowColumn flag not understood.
+           ValueError: row_col flag not understood.
         """
         if whiten is None:
             whiten = self.whiten
-        if rowColumn == 'c':
-            data = data.T[:]
-        elif rowColumn == 'r':
-            pass
+        if row_col == 'c':
+            full_data = data.T[:]
+        elif row_col == 'r':
+            full_data = data[:]
         else:
-            raise ValueError('Malformed rowColumn flag.')
+            raise ValueError('Malformed row_col flag.')
 
         cur_dim = data.shape[1]
         if whiten:
             iwm = np.diag(self.sValues)[:cur_dim,:cur_dim]
-            data = data.(iwm)
-        data = data.dot(self.eVectors[:curDim])
-        data += self.mean_vec[np.newaxis,:]
-        if rowColumn == 'r':
-            data = data
-        return data
+            full_data = full_data.dot(iwm)
+        full_data = full_data.dot(self.eVectors[:cur_dim])
+        full_data += self.mean_vec[np.newaxis,:]
+        if row_col == 'c':
+            full_data = full_data.T
+        return full_data
 
